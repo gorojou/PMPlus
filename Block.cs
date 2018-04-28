f5d62929f2e24e80fba9d6633fc19240432b0d31
Igor Miquilena <igor.miquilena@gmail.com>
2018-04-28 16:18:25 -0400

    Bloque genesis PMPlus desarrollado en C#
    Bloque genesis PMPlus desarrollado en C#

---
+++ Block.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockGenPMP_Demo
+{
+    public class Block
+    {
+        private int previosHash;
+        private String[] transactions;
+
+        private int blockHash;
+
+     
+        public Block(int previosHash, String[] transactions)
+        {
+            this.previosHash = previosHash;
+            this.transactions = transactions;
+
+            object[] contents = { transactions.GetHashCode(), previosHash };
+
+            this.blockHash = contents.GetHashCode();
+        }
+
+        public int getPreviosHash()
+        {
+            return previosHash;
+        }
+
+        public String[] getTransactions()
+        {
+            return transactions;
+        }
+
+        public int getblockHash()
+        {
+            return blockHash;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockGenPMP_Demo
+{
+    class Program
+    {
+        /*   Each block has: 
+         *   List of Transaction
+         *   Previous hash
+         *   Actual hash
+         
+         */
+        List<Block> blockchain = new List<Block>(); 
+
+        static void Main(string[] args)
+        {
+            string[] genesisTransaction = {"PMP Blockchain igor miquilena", "0 pmp begins" };
+
+            Block genesisBlock = new Block(0, genesisTransaction);
+
+            string[] block2Transaction = { "PMP Blockchain igor miquilena", "0 pmp begins" };
+
+            Block block2 = new Block(genesisBlock.getblockHash(), block2Transaction);
+
+
+            Console.WriteLine(genesisBlock.getblockHash());
+            Debug.Print("Hash genesis block");
+            Debug.Print(genesisBlock.getblockHash().ToString());
+            Console.WriteLine(block2.getblockHash());
+            Debug.Print("Hash block 2");
+            Debug.Print(block2.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
